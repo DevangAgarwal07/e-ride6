bbe65ee0265b4400a5cda293067cc23a2a1f459f
Devang Agarwal <pbmjaipur@gmail.com>
2022-03-17 22:02:56 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,12 +2,13 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDxDQpj9QgzcdMsK7NZ8hv3-P1RvCqrbEY",
-authDomain: "e-ride-b879a.firebaseapp.com",
-projectId: "e-ride-b879a",
-storageBucket: "e-ride-b879a.appspot.com",
-messagingSenderId: "139911666594",
-appId: "1:139911666594:web:941ffd7103a98591c870fb"
+  apiKey: "AIzaSyCZrG0BF6LrnRpWZjsPQxNGyMCLByo8HrM",
+  authDomain: "e-ride-549e4.firebaseapp.com",
+  projectId: "e-ride-549e4",
+  storageBucket: "e-ride-549e4.appspot.com",
+  messagingSenderId: "588298110700",
+  appId: "1:588298110700:web:34e3df331c6b1fc95047c9",
+  measurementId: "G-G3K7BWL5LH"
 };
 
 firebase.initializeApp(firebaseConfig);
